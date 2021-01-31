ae879cf69b2b7683c5d51a2ba0d25d12813ed65c
yee2542 <yee2542gtl@gmail.com>
2021-01-31 16:40:40 +0700

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   env: {
     es6: true,
     node: true,
   },
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "sonarjs"],
+  plugins: ['@typescript-eslint', 'sonarjs'],
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    "prettier/prettier": [
-      "warn",
+    'prettier/prettier': [
+      'warn',
       {
-        endOfLine: "auto",
+        endOfLine: 'auto',
       },
     ],
-    "sonarjs/cognitive-complexity": ["error", 6],
+    'sonarjs/cognitive-complexity': ['error', 6],
   },
 };
