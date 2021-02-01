08aec452ba566a3e984eea11ed1348635cac9710
yee2542 <yee2542gtl@gmail.com>
2021-02-02 00:44:38 +0700

    Add user interface

---
+++ src/user/enums/oauth-provider.enum.d.ts
@@ -0,0 +1,3 @@
+export enum OAuthProviderEnum {
+  facebook = 'facebook',
+}
---
+++ src/user/index.d.ts
@@ -0,0 +1,22 @@
+import { UserPackageEnum } from './enums/user-packages.enum';
+import { UserRoleEnum } from './enums/user-role.enum';
+import { UserZoneEnum } from './enums/user-zone';
+import { UserToken } from './user-token';
+
+export interface User {
+  mainEmail: string;
+  email: string[];
+  password: string;
+  oauth: UserToken[] | [];
+  name: string;
+  // logic
+  role: UserRoleEnum;
+  package: UserPackageEnum;
+
+  //   meta
+  zone: UserZoneEnum;
+  deactivate?: boolean;
+
+  createDate: Date;
+  updateDate: Date;
+}
