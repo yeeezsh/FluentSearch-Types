5ff45fcc20b29f1a5724a508f5d04e85dd4fdd88
yee2542 <yee2542gtl@gmail.com>
2021-02-04 22:27:18 +0700

    Patch File DTO (#10)
    * update file w/ insight
    
    * 0.2.1

--- types/file/dto/file-w-insight.ts
+++ types/file/dto/file-w-insight.ts
@@ -1,5 +1,7 @@
 import { BaseFileSchema, FileTypeEnum } from '..';
 import { InsightInfoDto } from '../../insight';
 
-export type FileWithInsightDto<T extends FileTypeEnum, M> = BaseFileSchema<T, M> &
-  InsightInfoDto & { uri: string };
+export type FileWithInsightDto<T extends FileTypeEnum, M> = BaseFileSchema<T, M> & {
+  uri: string;
+  insight?: InsightInfoDto[];
+};
