c89e59ed3aebdc4b937755acb1b64cf2a184d3a7
yee2542 <yee2542gtl@gmail.com>
2021-02-04 20:57:26 +0700

    add file dto

---
+++ types/file/dto/file-w-insight.ts
@@ -0,0 +1,5 @@
+import { BaseFileSchema, FileTypeEnum } from '..';
+import { InsightInfoDto } from '../../insight';
+
+export type FileWithInsightDto<T extends FileTypeEnum, M> = BaseFileSchema<T, M> &
+  InsightInfoDto;
