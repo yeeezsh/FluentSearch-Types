c6361ae4d3ac8571fc630f95c7262d96054b3dcf
yee2542 <yee2542gtl@gmail.com>
2021-02-04 20:25:43 +0700

    update insight schema

--- types/insight/schema/index.ts
+++ types/insight/schema/index.ts
@@ -1,6 +1,16 @@
-import { ModelEnum } from '..';
+import { Class, ModelEnum } from '..';
+import { LanguageEnum } from '../../common/lang.enum';
+import { BBox } from '../deepdetect-api';
 
 export type InsightSchema = {
+  _id: string;
   label: string;
+  result: Class['cat'];
   model: ModelEnum;
+  bbox?: BBox;
+  prob: Class['prob'];
+  lang: LanguageEnum;
+
+  createAt: Date;
+  updateAt: Date;
 };
