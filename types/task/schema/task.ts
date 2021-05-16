a7b0b7e1c971d0f7be2b6cc2c4040a7bf282ceb9
yee2542 <yee2542gtl@gmail.com>
2021-05-16 14:17:42 +0700

    add video task supported

--- types/task/schema/task.ts
+++ types/task/schema/task.ts
@@ -1,8 +1,14 @@
 import { BaseTypeSchema } from '.';
 import { ImageFileSchema, ImageThumbnailFileSchema } from '../..';
+import { VideoFileSchema } from '../../file';
+import { TaskTypeEnum } from '../enums';
 
 export type TaskSchema = BaseTypeSchema & {
+  type: TaskTypeEnum;
   group?: string;
   order: number;
-  file?: ImageFileSchema['_id'] | ImageThumbnailFileSchema['_id'];
+  file?:
+    | ImageFileSchema['_id']
+    | ImageThumbnailFileSchema['_id']
+    | VideoFileSchema['_id'];
 };
