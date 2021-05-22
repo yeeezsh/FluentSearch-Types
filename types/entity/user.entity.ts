9257adb7daf5e31d7e15ccae3be05b7660103097
yee2542 <yee2542gtl@gmail.com>
2021-05-23 05:02:28 +0700

    fill user entity

--- types/entity/user.entity.ts
+++ types/entity/user.entity.ts
@@ -1,6 +1,29 @@
-import { Schema } from 'mongoose';
+import { Document, Schema } from 'mongoose';
+import { UserRoleEnum, UserPackageEnum, UserZoneEnum, OAuthProviderEnum } from '..';
+import { UserSchema } from '../user';
 
-const userSchema = new Schema({
+export const userPackageConstant: UserPackageEnum[] = [
+  UserPackageEnum.freeUser,
+  UserPackageEnum.paidUser,
+];
+
+export const userRoleConstant: UserRoleEnum[] = [
+  UserRoleEnum.staff,
+  UserRoleEnum.admin,
+  UserRoleEnum.user,
+];
+
+export const oAuthProviderConstant: OAuthProviderEnum[] = [OAuthProviderEnum.facebook];
+
+export const userTokenSchema = new Schema(
+  {
+    provider: { type: String, required: true, enum: oAuthProviderConstant },
+    token: { type: String, required: true },
+  },
+  { _id: false },
+);
+
+const userSchema = new Schema<UserDocument>({
   mainEmail: { type: String, unique: true },
   email: { type: [String], required: true },
   password: { type: String, required: true },
@@ -21,3 +44,7 @@ const userSchema = new Schema({
   createDate: { type: Date, default: Date.now, required: true },
   updateDate: { type: Date, default: Date.now, required: true },
 });
+
+export type UserDocument = Document & UserSchema;
+
+export default userSchema;
