84c7c838c75c393f0635760dbce06f5259bda037
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-05-23 04:45:16 +0700

    File entity (#17)
    * add mongoose
    
    * add file entity
    
    * 0.7.0

---
+++ types/entity/file.entity.ts
@@ -0,0 +1,36 @@
+import { Document, Model, Schema, Types } from 'mongoose';
+import { FileTypeEnum, BaseFileSchema } from '..';
+
+const metaSchema = new Schema({
+  width: { type: Number },
+  height: { type: Number },
+  size: { type: Number },
+  contentType: { type: String },
+  extension: { type: String },
+});
+
+const fileSchema = new Schema<FileDocument>({
+  meta: { type: Object || metaSchema },
+  owner: { type: String, index: true },
+  zone: { type: String, index: true },
+  original_filename: { type: String },
+  type: { type: FileTypeEnum, index: true, enum: FileTypeEnum },
+
+  createAt: { type: Date, default: Date.now, index: true },
+  updateAt: { type: Date, default: Date.now, index: true },
+});
+
+export default fileSchema;
+
+export type FileDocument = Document &
+  BaseFileSchema<
+    FileTypeEnum.Image | FileTypeEnum.ImageThumbnail | FileTypeEnum.Video,
+    Record<'record', any>
+  >;
+
+export type File = BaseFileSchema<
+  FileTypeEnum.Image | FileTypeEnum.ImageThumbnail | FileTypeEnum.Video,
+  Record<'record', any>
+>;
+
+export const FILES_SCHEMA_NAME = 'files';
