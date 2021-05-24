c1ee8d3189b7343a9d1240ed910ca900a2433bea
yee2542 <yee2542gtl@gmail.com>
2021-05-24 07:51:12 +0700

    add uri

--- types/task/schema/task.ts
+++ types/task/schema/task.ts
@@ -1,13 +1,8 @@
 import { BaseTypeSchema } from '.';
-import { ImageFileSchema, ImageThumbnailFileSchema } from '../..';
-import { VideoFileSchema } from '../../file';
 import { TaskTypeEnum } from '../enums';
 
 export type TaskSchema = BaseTypeSchema & {
   type: TaskTypeEnum;
   group?: string;
-  file?:
-    | ImageFileSchema['_id']
-    | ImageThumbnailFileSchema['_id']
-    | VideoFileSchema['_id'];
+  uri?: string;
 };
