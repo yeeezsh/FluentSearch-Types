38c4d75c719cab1470ee455c0417f5f38c11ba80
yee2542 <yee2542gtl@gmail.com>
2021-05-27 05:37:04 +0700

    add insight entity

--- types/entity/index.ts
+++ types/entity/index.ts
@@ -2,3 +2,4 @@ export * from './file.entity';
 export * from './user.entity';
 export * from './task.entity';
 export * from './quota.entity';
+export * from './file.entity';
---
+++ types/entity/insight.entity.ts
@@ -0,0 +1,30 @@
+import { Document, Schema } from 'mongoose';
+import { FileTypeEnum } from '..';
+import { LanguageEnum } from '../common';
+import { InsightSchema, ModelEnum } from '../insight';
+
+const insightSchema = new Schema<InsightSchema>({
+  result: {
+    prob: { type: Number },
+    bbox: { type: Object },
+    cat: { type: String, index: true },
+  },
+  keyword: { type: String, index: true },
+
+  model: { type: ModelEnum, index: true },
+  bbox: { type: Object },
+  prob: { type: Number },
+  lang: { type: LanguageEnum },
+
+  fileId: { type: String, index: true },
+  fileType: { type: FileTypeEnum, index: true },
+  fps: { type: Number, index: true },
+
+  createAt: { type: Date, default: Date.now, index: true },
+  updateAt: { type: Date, default: Date.now, index: true },
+});
+
+export default insightSchema;
+
+export type InsightDocument = Document & InsightSchema;
+export const INSIGHT_SCHEMA_NAME = 'insights';
