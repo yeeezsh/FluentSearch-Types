30630bd946a58f2b51966d689da36af07ca5529f
yee2542 <yee2542gtl@gmail.com>
2021-05-27 00:31:52 +0700

    update model list

--- types/insight/enums/model.enum.ts
+++ types/insight/enums/model.enum.ts
@@ -1,4 +1,9 @@
 export enum ModelEnum {
-  detection_600 = 'detection_600',
   faces_emo = 'faces_emo',
+  detection_600 = 'detection_600',
+  ilsvrc_googlenet = 'ilsvrc_googlenet',
+  basic_fashion = 'basic_fashion',
+  classification_21k = 'classification_21k',
+  places = 'places',
+  vgg16 = 'vgg16',
 }
