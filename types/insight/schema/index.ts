076ef06b688bb792ea0273d0cf19552c5a68477e
yee2542 <yee2542gtl@gmail.com>
2021-05-27 05:27:45 +0700

    add filetype fileId and fps

--- types/insight/schema/index.ts
+++ types/insight/schema/index.ts
@@ -1,15 +1,20 @@
 import { ClassResponseAPI, ModelEnum } from '..';
 import { LanguageEnum } from '../../common/lang.enum';
+import { File } from '../../entity';
+import { FileTypeEnum } from '../../file';
 import { BBoxResponseAPI } from '../dto/deepdetect-api';
 
 export type InsightSchema = {
   _id: string;
-  fileId: string;
   result: ClassResponseAPI['cat'];
   model: ModelEnum;
   bbox?: BBoxResponseAPI;
   prob: ClassResponseAPI['prob'];
   lang: LanguageEnum;
+
+  fileId: File['_id'];
+  fileType: FileTypeEnum;
+  fps?: number;
 
   createAt: Date;
   updateAt: Date;
