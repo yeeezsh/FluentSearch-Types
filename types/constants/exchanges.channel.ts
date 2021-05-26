ea88803c1b8f95270bc05e169901088b14c0661a
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-05-26 08:11:54 +0700

    Ack queue (#24)
    * add ack exchange
    
    * add ack queue
    
    * 0.9.2

--- types/constants/exchanges.channel.ts
+++ types/constants/exchanges.channel.ts
@@ -1,3 +1,4 @@
 export const EXCHANGE_UPLOAD = 'upload_exchange';
 export const EXCHANGE_INSIGHT = 'insight_exchange';
 export const EXCHANGE_VIDEO_INSIGHT = 'insight_video_exchange';
+export const EXCHANGE_ACK_TASK = 'ack_task_exchange';
