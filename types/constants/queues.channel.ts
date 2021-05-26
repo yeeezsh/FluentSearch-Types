154f687946c0603d7d462ae86daa5da28a17668e
yee2542 <yee2542gtl@gmail.com>
2021-05-27 01:34:25 +0700

    add spotlight queue

--- types/constants/queues.channel.ts
+++ types/constants/queues.channel.ts
@@ -2,3 +2,4 @@ export const ADMISSION_QUEUE = 'admission_queue';
 export const WORKER_INSIGHT_QUEUE = 'worker_insight_queue';
 export const WORKER_VIDEO_INSIGHT_QUEUE = 'worker_video_insight_queue';
 export const ACK_TASK_QUEUE = 'ack_task_queue';
+export const SPOTLIGHT_QUEUE = 'spotlight_queue';
