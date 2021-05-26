d7ce3434d57a2d7a1d509ced96520931d8be749e
yee2542 <yee2542gtl@gmail.com>
2021-05-27 00:34:01 +0700

    add model available

--- types/constants/index.ts
+++ types/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './queues.channel';
 export * from './exchanges.channel';
+export * from './model.available';
---
+++ types/constants/model.available.ts
@@ -0,0 +1,11 @@
+import { ModelEnum } from '../insight';
+
+export const MODEL_AVAILABLE_NAME: ModelEnum[] = [
+  ModelEnum.faces_emo,
+  ModelEnum.detection_600,
+  ModelEnum.ilsvrc_googlenet,
+  ModelEnum.basic_fashion,
+  ModelEnum.classification_21k,
+  ModelEnum.places,
+  ModelEnum.vgg16,
+];
