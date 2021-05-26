fb2d507565e10ef700cac8878e287cfa18208051
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-05-27 05:37:54 +0700

    Insight queue (#28)
    * add spotlight queue
    
    * add filetype fileId and fps
    
    * add insight entity
    
    * add keyword
    
    * 0.9.6

--- types/insight/schema/index.ts
+++ types/insight/schema/index.ts
@@ -1,15 +1,23 @@
 import { ClassResponseAPI, ModelEnum } from '..';
 import { LanguageEnum } from '../../common/lang.enum';
+import { File } from '../../entity';
+import { FileTypeEnum } from '../../file';
 import { BBoxResponseAPI } from '../dto/deepdetect-api';
 
 export type InsightSchema = {
   _id: string;
-  fileId: string;
+
   result: ClassResponseAPI['cat'];
+  keyword: string;
+
   model: ModelEnum;
   bbox?: BBoxResponseAPI;
   prob: ClassResponseAPI['prob'];
   lang: LanguageEnum;
+
+  fileId: File['_id'];
+  fileType: FileTypeEnum;
+  fps?: number;
 
   createAt: Date;
   updateAt: Date;
