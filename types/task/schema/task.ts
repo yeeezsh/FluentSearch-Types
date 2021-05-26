1a84163ae88a645f3be8b97a9c45d633ef5217c6
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-05-26 21:20:04 +0700

    Hofix (#26)
    * fix bad filetype refs
    
    * 0.9.4

--- types/task/schema/task.ts
+++ types/task/schema/task.ts
@@ -1,9 +1,10 @@
 import { BaseTypeSchema } from '.';
 import { File } from '../../entity';
+import { FileTypeEnum } from '../../file';
 
 export type TaskSchema = BaseTypeSchema & {
   group?: string;
   uri?: string;
   fileId?: File['_id'];
-  fileType?: File['type'];
+  fileType?: FileTypeEnum;
 };
