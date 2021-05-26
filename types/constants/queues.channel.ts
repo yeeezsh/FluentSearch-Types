3a86029f0326e21d494433f3a66c7cc634c1a0f8
yee2542 <yee2542gtl@gmail.com>
2021-05-26 08:11:08 +0700

    add ack queue

--- types/constants/queues.channel.ts
+++ types/constants/queues.channel.ts
@@ -1,3 +1,4 @@
 export const ADMISSION_QUEUE = 'admission_queue';
 export const WORKER_INSIGHT_QUEUE = 'worker_insight_queue';
 export const WORKER_VIDEO_INSIGHT_QUEUE = 'worker_video_insight_queue';
+export const ACK_TASK_QUEUE = 'ack_task_queue';
